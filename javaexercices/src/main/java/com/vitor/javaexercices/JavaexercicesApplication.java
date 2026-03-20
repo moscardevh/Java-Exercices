f19ba73e72f05b53ae65f91b23c8303614b840ae
moscardevh <moscardev@gmail.com>
2026-03-20 12:46:56 -0300

    iniciado projeto de exercicios em java spring e js

---
+++ javaexercices/src/main/java/com/vitor/javaexercices/JavaexercicesApplication.java
@@ -0,0 +1,13 @@
+package com.vitor.javaexercices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaexercicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaexercicesApplication.class, args);
+	}
+
+}
---
+++ javaexercices/src/test/java/com/vitor/javaexercices/JavaexercicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitor.javaexercices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaexercicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
